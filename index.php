7651a2708544113f2e86f8508c36110be446aee3
Kostya Shelko <kostynru@ymail.com>
2013-06-15 21:06:57 +0700

    Human added txt ext

--- index.php
+++ index.php
@@ -3,5 +3,7 @@
     <link rel="author" href="humans.txt" />
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
+    <body>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery-2.0.1.min.js"></script>
+    </body>
