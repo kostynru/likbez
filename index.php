47509fd5f0e75e3db61c6a257bbea0e492aab6dd
Kostya Shelko <kostynru@ymail.com>
2013-06-15 21:02:53 +0700

    Human txt has changed

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <head>
+    <link rel="author" href="humans.txt" />
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-
 </head>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-2.0.1.min.js"></script>
